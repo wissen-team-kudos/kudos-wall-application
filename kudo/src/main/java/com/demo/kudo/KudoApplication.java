6449e1fa02215abe0122e80836e9e1d2f1afed2f
wissen-kudos-team <kudoswallapplication@gmail.com>
2020-07-10 15:12:39 +0530

    Initialized project

---
+++ kudo/src/main/java/com/demo/kudo/KudoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.kudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KudoApplication.class, args);
+	}
+
+}
---
+++ kudo/src/test/java/com/demo/kudo/KudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.kudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
