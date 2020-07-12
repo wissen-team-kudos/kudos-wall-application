1e35f61690af2be95f731d2586226bdda0db8664
wissen-team-kudos <kudoswallapplication@gmail.com>
2020-07-12 13:36:07 +0530

    Updated groups to groups_of_user

--- Sql_Scripts/kudos_db.sql
+++ Sql_Scripts/kudos_db.sql
@@ -12,10 +12,10 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-DROP TABLE IF EXISTS `groups`;
-CREATE TABLE `groups` (
+DROP TABLE IF EXISTS `groups_of_user`;
+CREATE TABLE `groups_of_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `groupnname` varchar(50) NOT NULL,
+  `groupname` varchar(50) NOT NULL,
   `password` varchar(68) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -33,14 +33,16 @@ DROP TABLE IF EXISTS `users_groups`;
 CREATE TABLE `users_groups` (
   `user_id` int(11) NOT NULL,
   `group_id` int(11) NOT NULL,
+  primary key (`user_id`, `group_id`),
   foreign key (`user_id`) references users (`id`) on delete cascade on update cascade,
-  foreign key (`group_id`) references groups (`id`) on delete cascade on update cascade
+  foreign key (`group_id`) references `groups_of_user` (`id`) on delete cascade on update cascade
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `users_kudos`;
 CREATE TABLE `users_kudos` (
   `user_id` int(11) NOT NULL,
   `kudo_id` int(11) NOT NULL,
+  primary key (`user_id`, `kudo_id`),
   foreign key (`user_id`) references users (`id`) on delete cascade on update cascade,
   foreign key (`kudo_id`) references kudos (`id`) on delete cascade on update cascade
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -49,6 +51,7 @@ DROP TABLE IF EXISTS `groups_kudos`;
 CREATE TABLE `groups_kudos` (
   `group_id` int(11) NOT NULL,
   `kudo_id` int(11) NOT NULL,
-  foreign key (`group_id`) references groups (`id`) on delete cascade on update cascade,
+  primary key (`group_id`, `kudo_id`),
+  foreign key (`group_id`) references `groups_of_user` (`id`) on delete cascade on update cascade,
   foreign key (`kudo_id`) references kudos (`id`) on delete cascade on update cascade
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
