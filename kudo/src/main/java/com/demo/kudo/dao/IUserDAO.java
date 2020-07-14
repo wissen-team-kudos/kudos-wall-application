a63be88538c103d178bd1d88108d32e9022a541c
wissen-team-kudos <kudoswallapplication@gmail.com>
2020-07-14 10:02:34 +0530

    Added method to get user using username

--- kudo/src/main/java/com/demo/kudo/dao/IUserDAO.java
+++ kudo/src/main/java/com/demo/kudo/dao/IUserDAO.java
@@ -12,6 +12,8 @@ public interface IUserDAO {
 	public User saveUser(User theUser);
 
 	public User getUser(int theId);
+	
+	public User getUser(String username);
 
 	public void deleteUser(int theId);
 }
