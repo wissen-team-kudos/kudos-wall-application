50803dd117bfe7f8ccc8d8a1200942fa92ecd333
wissen-team-kudos <kudoswallapplication@gmail.com>
2020-07-14 09:59:36 +0530

    added UNIQUE constraint to username attribute

--- Sql_Scripts/kudos_db.sql
+++ Sql_Scripts/kudos_db.sql
@@ -6,7 +6,7 @@ USE `kudos_db`;
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL unique,
   `password` varchar(68) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
