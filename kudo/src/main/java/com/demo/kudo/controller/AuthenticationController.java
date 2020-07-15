c55e260e7b018d274e401114d03c858112310ee5
wissen-team-kudos <kudoswallapplication@gmail.com>
2020-07-15 08:46:39 +0530

    Added Cross Origin for localhost:4200

--- kudo/src/main/java/com/demo/kudo/controller/AuthenticationController.java
+++ kudo/src/main/java/com/demo/kudo/controller/AuthenticationController.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,7 +34,8 @@ public class AuthenticationController {
 	
 	@Autowired
 	private UserService userService;
-
+	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping("/authenticate")
 	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
 		try {
