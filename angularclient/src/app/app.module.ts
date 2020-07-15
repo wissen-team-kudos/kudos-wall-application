98eab3189ce3b6b80e7a0689432c365462c8e8c0
wissen-team-kudos <kudoswallapplication@gmail.com>
2020-07-15 08:50:15 +0530

    defined various imports and providers for login page, routing and authorization

--- angularclient/src/app/app.module.ts
+++ angularclient/src/app/app.module.ts
@@ -1,3 +1,7 @@
+import { LoginGuard } from './services/login-guard.service';
+import { AuthGuard } from './services/auth-guard.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { AuthenticationService } from './services/authentication.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -8,21 +12,29 @@ import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 import { NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import { GroupModalComponent } from './group-modal/group-modal.component';
 import { FormsModule } from "@angular/forms";
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     BsNavbarComponent,
-    GroupModalComponent
+    GroupModalComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    HttpClient,
+    AuthenticationService,
+    AuthGuard,
+    LoginGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
