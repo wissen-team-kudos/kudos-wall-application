34e832422818d4d7b1bcaafc63918dd1358950b6
wissen-team-kudos <kudoswallapplication@gmail.com>
2020-07-15 08:53:53 +0530

    login guard to prevent repetitive access to login page

---
+++ angularclient/src/app/services/login-guard.service.ts
@@ -0,0 +1,26 @@
+import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
+import { AuthenticationService } from './authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginGuard implements CanActivate {
+
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router
+    ) { }
+
+  canActivate() {
+    if(this.authService.isLoggedIn()){
+      this.router.navigate(['/']);
+      return false; 
+    }
+    return true;
+    
+  }
+
+
+}
