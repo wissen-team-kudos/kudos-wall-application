974b2d1ac20d4b548de5a67879e747de50b139a9
wissen-team-kudos <kudoswallapplication@gmail.com>
2020-07-16 15:49:25 +0530

    Updated for testing of user service. Updates are commented

--- angularclient/src/app/home/home.component.ts
+++ angularclient/src/app/home/home.component.ts
@@ -1,3 +1,5 @@
+import { UserService, User } from './../services/user.service';
+import { AuthenticationService } from './../services/authentication.service';
 import { Component, OnInit } from '@angular/core';
 import { SampleGroupService } from '../dummy-services/sample-group.service';
 import { SampleKudoService } from '../dummy-services/sample-kudo.service';
@@ -15,7 +17,8 @@ export class HomeComponent implements OnInit {
   kudos:String[];
   groups:String[];
 
-  constructor(private sampleGroupService: SampleGroupService,private sampleKudoService: SampleKudoService) {
+  constructor(private sampleGroupService: SampleGroupService,private sampleKudoService: SampleKudoService,
+    private userService:UserService) {
 
     this.kudos= this.sampleKudoService.getKudos();
    }
@@ -25,7 +28,22 @@ export class HomeComponent implements OnInit {
   
   showKudos(){
     this.clicked=true;
-    
+    /*Author: Mandar --- (For testing User service)
+    this.userService.getUser(1);
+    this.userService.getAllUsers();
+    let user : User = {
+      username : 'user4',
+      password : 'pass4'
+    };
+    this.userService.addUser(user);
+    let userToUpdate : User = {
+      id : 6,
+      username : 'user4',
+      password : 'pass5'
+    }
+    this.userService.updateUser(userToUpdate);
+    this.userService.deleteUser(6);
+    */
     this.kudos= this.sampleKudoService.getKudos();
     console.log(this.kudos)
   }
