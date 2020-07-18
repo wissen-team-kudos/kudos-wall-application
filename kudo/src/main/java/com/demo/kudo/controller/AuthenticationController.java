7da513f0ab9f7f64c05c430f44a253497db2d31c
wissen-team-kudos <kudoswallapplication@gmail.com>
2020-07-18 11:07:13 +0530

    401 error thrown instead of 403. 401 will be caught

--- kudo/src/main/java/com/demo/kudo/controller/AuthenticationController.java
+++ kudo/src/main/java/com/demo/kudo/controller/AuthenticationController.java
@@ -1,11 +1,14 @@
 package com.demo.kudo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,8 +41,9 @@ public class AuthenticationController {
 			authenticationManager.authenticate(
 					new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
 			);
-		} catch(BadCredentialsException e) {
-			throw new Exception("Incorrect username or password ", e);
+		} catch(BadCredentialsException | InternalAuthenticationServiceException e) {
+			System.out.println(e);
+			return new ResponseEntity<String>("Invalid username or password", HttpStatus.UNAUTHORIZED);
 		}
 		
 		final UserDetails userDetails = userDetailsService
