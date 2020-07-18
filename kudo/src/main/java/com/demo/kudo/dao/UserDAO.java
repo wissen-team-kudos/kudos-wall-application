fa77f430a33c374307e5124cf7a9c54e02a5f56f
sayali1102 <sayalijadhav110297@gmail.com>
2020-07-18 14:24:22 +0530

    fixed import

--- kudo/src/main/java/com/demo/kudo/dao/UserDAO.java
+++ kudo/src/main/java/com/demo/kudo/dao/UserDAO.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import javax.persistence.EntityManager;
@@ -87,7 +86,7 @@ public class UserDAO implements IUserDAO {
 		currentSession.saveOrUpdate(theUser);	
 		
 		return theUser;
-	}
+	}	
 	
 	@Override
 	public void deleteUser(int theId) {
