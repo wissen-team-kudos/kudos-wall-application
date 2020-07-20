46b91114d801762d4ed2e3c7103cd200ace61227
wissen-team-kudos <kudoswallapplication@gmail.com>
2020-07-20 14:18:39 +0530

    Added form validation

--- angularclient/src/app/kudo-modal/kudo-modal.component.ts
+++ angularclient/src/app/kudo-modal/kudo-modal.component.ts
@@ -1,6 +1,6 @@
 import { Group } from './../models/group';
-import { Component, OnInit, Input } from '@angular/core';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit, Input, ViewChild, TemplateRef } from '@angular/core';
+import { NgbModal, ModalDismissReasons, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import {NgForm} from '@angular/forms';
 import { KudosService } from '../services/kudos.service';
 import { Kudos} from '../models/kudos';
@@ -19,10 +19,14 @@ export class KudoModalComponent implements OnInit {
     user : {id: number, username: string, password: string},
     checked: boolean
   }[];
+  isUserListEmpty: boolean;
+  modalRef : NgbModalRef;
+  @ViewChild('content', { static: true }) content;
 
   constructor(private modalService: NgbModal, private kudoService: KudosService) { }
 
   ngOnInit(): void {
+    this.isUserListEmpty = false;
     this.userlist = new Array();
     console.log(this.theGroup)
     for (let index = 0; index < this.theGroup.users.length; index++) {
@@ -30,8 +34,13 @@ export class KudoModalComponent implements OnInit {
     }
   }
 
+  ngAfterViewInit() {
+    // this.content is NOW valid !!
+ }
+
   open(content) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+    this.modalRef = this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'});
+    this.modalRef.result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
@@ -53,11 +62,25 @@ export class KudoModalComponent implements OnInit {
     console.log(this.selectedOptions);
     // this.theKudo.content=form.value.name;
     // this.kudoService.addKudo(this.theKudo);
+    for (let index = 0; index < this.theGroup.users.length; index++) {
+      this.userlist[index] = {user : this.theGroup.users[index], checked : false};
+    }
   }
 
   get selectedOptions() { // right now: ['1','3']
     return this.userlist
               .filter(opt => opt.checked)
               .map(opt => opt.user)
+  }
+
+  testIfEmpty(){
+    if(this.selectedOptions.length == 0) {
+      this.isUserListEmpty = true;
+    }
+    else{
+      this.isUserListEmpty = false;
+      this.modalRef.close();
+      this.open(this.content);
+    }
   }
 }
