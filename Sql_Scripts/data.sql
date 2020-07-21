031689ac04bac3949a9ce15097ef8bf00ed44c51
wissen-team-kudo <kudoswallapplication@gmail.com>
2020-07-21 17:34:03 +0530

    Modified 'group' to 'room' in scripts

--- Sql_Scripts/data.sql
+++ Sql_Scripts/data.sql
@@ -3,16 +3,16 @@ insert into users values
 	(2,'user2','pass2'),
 	(3,'user3','pass3');
 
-insert into `groups_of_user` values
-	(1,'group1','group1'),
-	(2,'group2','group2');
+insert into `rooms_of_user` values
+	(1,'room1','room1'),
+	(2,'room2','room2');
 
 insert into kudos values
 	(1,'kudos1',1),
 	(2,'kudos2',2),
 	(3,'kudos3',3);
 
-insert into users_groups values
+insert into users_rooms values
 	(1,1),
 	(1,2),
 	(2,1),
@@ -26,7 +26,7 @@ insert into users_kudos values
     (3,2),
     (3,1);
     
-insert into groups_kudos values
+insert into rooms_kudos values
 	(1,2),
 	(2,3),
 	(1,1),
